120aedea05fe05c969fefb907ae076fa7d9b9301
gbiagomba <gbiago961@gmail.com>
2019-04-16 18:16:37 -0400

    Made Git_Init.sh executable

--- rsc/Git_Init.sh
+++ rsc/Git_Init.sh
@@ -10,6 +10,16 @@ echo "What is your git username?"
 echo "I need this for your project"
 read usrnm
 
+# Asking the user for their real name
+echo "What is your git username?"
+echo "I need this for your project"
+read name
+
+# Asking the user for their email
+echo "What is your git username?"
+echo "I need this for your project"
+read email
+
 # Creating workspace and readme file
 mkdir /opt/$prj/
 touch /opt/$prj/README.md
@@ -19,6 +29,8 @@ echo "# First commit" > README.md
 cd /opt/$prj/
 git init --bare
 git add README.md
+git config --global user.name "$name"
+git config --global user.email $email
 git commit -m "First commit"
 git tag -a "v1.0.0" -m "version 1.0.0"
 git remote add origin https://github.com/$usrnm/$prj.git
